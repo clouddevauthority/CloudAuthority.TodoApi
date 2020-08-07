a60f86cb2c6d0d49c0e519d2b8da98723ca6f94f
Pratik Khasnabis <pratik.khasnabis@outlook.com>
2020-08-07 12:41:22 +1000

    HealthCheck endpoint now takes a key as an input.

--- src/api/Softveda.Todo.Api.InMemory/Controllers/HealthController.cs
+++ src/api/Softveda.Todo.Api.InMemory/Controllers/HealthController.cs
@@ -11,23 +11,32 @@ namespace Softveda.Todo.Api.InMemory.Controllers
 	{
 		private readonly ILogger<HealthController> _logger;
 		private readonly IConfiguration _configuration;
+		private readonly string HealthCheckKey;
 
-    public HealthController(ILogger<HealthController> logger, IConfiguration configuration)
+		public HealthController(ILogger<HealthController> logger, IConfiguration configuration)
 		{
 			_logger = logger;
 			_configuration = configuration;
-    }
+			HealthCheckKey = _configuration.GetSection("HealthCheckKey").Value;
+		}
 
 		/// <summary>
 		/// Perform a Health check
 		/// </summary>
+		/// <param name="key">A key that must be provided to this endpoint</param>
 		/// <returns>Status code</returns>
 		/// <response code="200">If the health check is successfull</response>
-		[HttpGet(Name = "HealthCheck")]
+		/// <response code="403">If the key is incorrect</response>
+		[HttpGet("{key}", Name = "HealthCheck")]
 		[ProducesResponseType(StatusCodes.Status200OK)]
-		public ActionResult<string> GetHealthCheck()
+		[ProducesResponseType(StatusCodes.Status403Forbidden)]
+		public ActionResult<string> GetHealthCheck([FromRoute] string key)
 		{
 			_logger.LogInformation("Checking Health of Application");
+			if (string.CompareOrdinal(key, HealthCheckKey) != 0)
+			{
+				return Forbid();
+			}
 
 			return Ok("HealthCheck OK");
 		}
